95d24f1f51570e8dbc4e887109c685891a9238e6
tkaria <tkaria.info@gmail.com>
2020-11-02 04:02:45 +0900

    add unity proj

---
+++ Assets/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up, Time.deltaTime * 80.0f);
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label("Build PC Standalone application to Export folder, and name the executable as Child.exe.");
+	}
+}
---
+++ ReadMe.txt
@@ -0,0 +1,9 @@
+EmbeddeWindow example
+
+Quickstart
+* Open Unity project
+* Build Windows Standalone application to Export directory, and name the executable as Child.exe
+* Run Container.exe in Export directory
+* You should see Windows Standalone application running inside Container application
+
+Container.exe sources are located Container folder, you need Visual Studio 2010 or higher to build it.
